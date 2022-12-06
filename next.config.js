c26effcb251a52a9076b383a461a1b5683a128ce
Kyrillus Mehanni <99624083+Kyrillus@users.noreply.github.com>
2022-12-06 10:07:17 +0100

    add cv to /cv path

--- next.config.js
+++ next.config.js
@@ -1,5 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: '/cv',
+        destination: '/cv.pdf',
+        permanent: true,
+      },
+    ]
+  },
   reactStrictMode: true,
   swcMinify: true,
 }
